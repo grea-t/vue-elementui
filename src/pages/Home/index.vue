7224a29ad9ddaef9dd6875c5f5795bbd7dc52d0d
grea-t <1193976273@qq.com>
2021-01-07 15:08:17 +0800

    创建项目

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Nav/>
+    Home
+  </div>
+</template>
+
+<script>
+  import Nav from '../../components/Nav'
+
+  export default {
+    name: "Home",
+    components: {
+      Nav
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../pages/Home'
+import Mine from '../pages/Mine'
+import Search from '../pages/Search'
+import SongList from '../pages/SongList'
+import Artists from '../pages/Artists'
+import Top from '../pages/Top'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Home
+    }, {
+      path: '/mine',
+      component: Mine
+    }, {
+      path: '/search',
+      component: Search
+    }, {
+      path: '/songList',
+      component: SongList
+    }, {
+      path: '/artists',
+      component: Artists
+    }, {
+      path: '/top',
+      component: Top
+    }
+  ]
+})
+
